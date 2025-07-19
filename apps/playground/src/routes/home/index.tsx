165ef7145b2d7ac46093a1436889b8c744a2fc06
vishnumallela <mallelavishnuchoudary@gmail.com>
2025-07-19 13:43:36 -0500

    tanstack-playground-config

---
+++ apps/playground/src/routes/home/index.tsx
@@ -0,0 +1,13 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/home/")({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-400 to-purple-600 flex items-center justify-center">
+      Home
+    </div>
+  );
+}
---
+++ apps/playground/src/routes/index.tsx
@@ -0,0 +1,20 @@
+// src/routes/index.tsx
+import { createFileRoute } from '@tanstack/react-router'
+import { useState } from 'react'
+
+export const Route = createFileRoute('/')({
+  component: Home,
+})
+
+function Home() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <button
+      type="button"
+      onClick={() => setCount(count + 1)}
+    >
+      Add 1 to {count}?
+    </button>
+  )
+}
